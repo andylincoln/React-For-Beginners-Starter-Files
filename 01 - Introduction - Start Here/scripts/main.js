dfeb95f33496bd8b266218bc732e102a0ec6d3df
Andy Lincoln <andrewlincoln11@gmail.com>
2016-08-31 11:49:56 -0400

    First component

---
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -0,0 +1,26 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+/*
+ * StorePicker
+ * This will let us make <StorePicker/>
+ */
+
+var StorePicker = React.createClass({
+
+  render: function() {
+    var name = "wes";
+    // Normal Comments
+    return (
+      <form className="store-selector">
+        {/*Comments go here*/}
+        <h2>Please Enter a Store {name}</h2>
+        <input type="text" ref="storeId" required/>
+        <input type="Submit"/>
+      </form>
+    )
+  }
+
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector("#main"));
