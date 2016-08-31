474bcd9e1da8961617f3e24a8a39a20e514f736f
Andy Lincoln <andrewlincoln11@gmail.com>
2016-08-31 18:53:26 -0400

    Router changes, for real this time

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -1,6 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require("react-router");
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require("history/lib/createBrowserHistory");
+
 /*
   App
 */
@@ -84,4 +90,15 @@ var StorePicker = React.createClass({
 
 });
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+/*
+ * Routes
+ */
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+  </Router>
+);
+
+
+ReactDOM.render(routes, document.querySelector('#main'));
