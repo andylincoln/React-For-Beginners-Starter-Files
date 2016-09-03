b9735aeff818af645b036c6e992e9b60778f9238
Andy Lincoln <andrewlincoln11@gmail.com>
2016-09-03 12:56:06 -0400

    Chapter 17

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -223,9 +223,18 @@ var Order = React.createClass({
 */
 var Inventory = React.createClass({
   renderInventory : function(key) {
+    var linkState = this.props.linkState;
     return (
       <div className="fish-edit" key={key}>
         <input type="text" valueLink={linkState("fishes." + key + ".name")}/>
+        <input type="text" valueLink={linkState("fishes." + key + ".price")}/>
+        <select valueLink={linkState("fishes." + key + ".status")}>
+          <option value="unavailable">Sold Out!</option>
+          <option value="available">Fresh!</option>
+        </select>
+        <textarea valueLink={linkState("fishes." + key + ".desc")}></textarea>
+        <input type="text" valueLink={linkState("fishes." + key + ".image")}/>
+        <button>Remove Fish</button>
       </div>
     );
   },
