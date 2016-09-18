ac37db14b0698cbcaf4cefb7270ff73ab5aaf9ae
Andy Lincoln <andrewlincoln11@gmail.com>
2016-09-18 12:34:55 -0400

    Chapter 24

--- 01 - Introduction - Start Here/scripts/components/StorePicker.js
+++ 01 - Introduction - Start Here/scripts/components/StorePicker.js
@@ -7,15 +7,17 @@ import React from 'react';
 import { History } from 'react-router';
 import h from '../helpers';
 
-var StorePicker = React.createClass({
-  mixins : [History],
-  goToStore : function(event) {
+class StorePicker extends React.Component {
+
+  mixins : [History]
+
+  goToStore(event) {
     event.preventDefault();
     // get the data from the input
     var storeId = this.refs.storeId.value;
     this.history.pushState(null, '/store/' + storeId);
-  },
-  render : function() {
+  }
+  render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
@@ -24,7 +26,8 @@ var StorePicker = React.createClass({
       </form>
     )
   }
+}
 
-});
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;
